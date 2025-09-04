61e73a017ecd0fac6887dededf0c25cd149f5765
Jaxson Brown <jokesene@outlook>
2025-09-04 17:58:33 +0800

    added append

--- practice/linkedlist.js
+++ practice/linkedlist.js
@@ -1,18 +1,37 @@
-const node1 = {
-    data: "example1",
-    next: null,
+class Node {
+    constructor(data, next) {
+        this.data = data;
+        this.next = next;
+    }
 }
 
-const node2 = {
-    data: "example2",
-    next: null,
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    // METHOD: Add a new node to the list with the given data
+    append(data) {
+        // create a new node
+        const node = new Node(data, null);
+
+        // set up reference values
+        if (this.size == 0) {   // empty list case
+            this.head = node;
+        } else {
+            let currentNode = this.head;
+            while (currentNode.next != null) {
+                currentNode = currentNode.next;
+            }
+            currentNode.next = node;
+        }
+        
+        this.size ++;
+    }
 }
 
-const node3 = {
-    data: 123,
-    next: null
-}
-
-node1.next = node2;
-node2.next = node3;
-console.log(node1.next.next.next);
+const linkedList = new LinkedList();
+linkedList.append("Hello World");
+linkedList.append(123);
+console.log(linkedList.head.data);
