7d1b08c90cd10e9597ca915205c726e355114e2e
Jaxson Brown <jokesene@outlook>
2025-09-15 15:40:34 +0800

    started blockchain

---
+++ practice/blockchain.js
@@ -0,0 +1,20 @@
+const crypto = require("crypto");
+
+class Block {
+    constructor(index, transactions, prevHash) {
+        this.index = index;
+        this.transactions = transactions;
+        this.prevHash = prevHash;
+        this.nonce = 0;
+        this.hash = createHash();
+    }
+
+    createHash() {
+        const data = this.index + JSON.stringify(this.transactions) + this.prevHash + this.nonce;
+        const hash = crypto.createHash("sha256").update(data).digest("hex");
+
+        return hash;
+    }
+}
+
+const testBlock = new Block(1, "test transaction", "testprevhash");
