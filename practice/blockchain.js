2e27ab7b17820801471f98dbd0f9410bca059903
Jaxson Brown <jokesene@outlook>
2025-09-15 16:07:30 +0800

    added mining

--- practice/blockchain.js
+++ practice/blockchain.js
@@ -1,6 +1,7 @@
 const crypto = require("crypto");
 
 class Block {
+    // define constructor to set up properties
     constructor(index, transactions, prevHash) {
         this.index = index;
         this.transactions = transactions;
@@ -9,11 +10,29 @@ class Block {
         this.hash = this.createHash();
     }
 
+    // a method to return a hash based on all the properties of the Block
     createHash() {
         const data = this.index + JSON.stringify(this.transactions) + this.prevHash + this.nonce;
         const hash = crypto.createHash("sha256").update(data).digest("hex");
 
         return hash;
     }
+
+    // a method to "mine" the block based on a given difficulty
+    mineBlock(difficulty) {
+        const target = "0".repeat(difficulty);
+
+        while (this.hash.substring(0, difficulty) != target) {
+            // change the nonce to give a new hash each iteration
+            this.nonce ++;
+            this.hash = this.createHash();
+            console.log("Current hash: " + this.hash);
+        }
+        console.log("Block mined! Hash: " + this.hash);
+    }
 }
+
 const testBlock = new Block(1, "test transaction", "testprevhash");
+testBlock.mineBlock(5);
+
+console.log(testBlock);
